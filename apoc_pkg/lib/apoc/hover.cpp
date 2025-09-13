597cad66c47a6956fd08fa1fd0d1442a780f95c2
cAtPile <799818193@qq.com>
2025-09-13 06:15:54 -0700

    2115

--- apoc_pkg/lib/apoc/hover.cpp
+++ apoc_pkg/lib/apoc/hover.cpp
@@ -16,6 +16,7 @@
 */
 
 #include "apoc_pkg/apoc.h"
+/*
 
 bool apoc::hoverSwitch(float hover_time){
 
@@ -52,4 +53,40 @@ bool apoc::hoverSwitch(float hover_time){
     ROS_ERROR("ROS node shutdown during hover");
     return false;
 
+}*/
+bool apoc::hoverSwitch(float hover_time){
+
+    geometry_msgs::PoseStamped hover_pose;
+    // 设置正确的坐标帧
+    hover_pose.header.frame_id = "map";  // 或根据你的系统使用"local_origin"
+    
+    // 获取初始悬停位置
+    hover_pose.pose.position.x = current_pose.pose.position.x;
+    hover_pose.pose.position.y = current_pose.pose.position.y;
+    hover_pose.pose.position.z = current_pose.pose.position.z;
+    hover_pose.pose.orientation = current_pose.pose.orientation;
+
+    ros::Time start = ros::Time::now();
+    // 使用较高的控制频率，建议至少30Hz
+    ros::Rate hover_rate(50);  // 50Hz的发布频率
+
+    while (ros::ok()) {
+        // 检查悬停时间是否已到
+        if ((ros::Time::now() - start).toSec() >= hover_time) {
+            return true;
+        }
+        
+        // 更新时间戳（非常重要，否则MAVROS会忽略过期指令）
+        hover_pose.header.stamp = ros::Time::now();
+        
+        // 发布悬停位置指令
+        local_pos_pub.publish(hover_pose);
+        
+        ros::spinOnce();
+        hover_rate.sleep();
+    }
+
+    // ROS节点异常退出
+    ROS_ERROR("ROS node shutdown during hover");
+    return false;
 }
