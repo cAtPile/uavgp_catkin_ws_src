72de6088299f7995b055405698b90ad3575d2415
cAtPile <799818193@qq.com>
2025-09-14 17:29:09 +0800

    trace

--- apoc_pkg/include/apoc_pkg/track.h
+++ apoc_pkg/include/apoc_pkg/track.h
@@ -1,1 +1,24 @@
 //
+#ifndef TRACK_H
+#define TRACK_H
+
+#include <ros/ros.h>//ros
+#include <geometry_msgs/PoseStamped.h>//mav定点
+#include <mavros_msgs/State.h>//状态
+#include <tf2/LinearMath/Quaternion.h>//四元数
+#include <tf2/LinearMath/Matrix3x3.h>//rpy
+
+#include <vector>
+#include <cmath>
+
+#include <geometry_msgs/TwistStamped.h>//mav速度
+
+class track{
+private:
+    ros::Subscriber trace_sub;
+
+public:
+
+};
+
+#endif
