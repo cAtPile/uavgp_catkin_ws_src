f133bcffd31eb91075fa163ef5336e0dbb1ffaca
cAtPile <799818193@qq.com>
2025-09-18 06:39:34 -0700

    1

--- apoc_pkg/script/tracexdebug.py
+++ apoc_pkg/script/tracexdebug.py
@@ -14,7 +14,7 @@ def simple_publisher():
     while not rospy.is_shutdown():
         msg = detection_data()
         msg.detection_id = 1  # 强制有目标
-        msg.detection_x = 640.0  # 固定坐标
+        msg.detection_x = 341.0  # 固定坐标
         msg.detection_y = 320.0
         pub.publish(msg)
         rospy.loginfo(f"有目标 | id=1, x=341.0, y=320.0")
