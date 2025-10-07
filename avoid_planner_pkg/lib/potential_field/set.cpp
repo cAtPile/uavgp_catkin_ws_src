a5cc44e7789d202e0c17b5ada4b663cfa38e7bd5
cAtPile <799818193@qq.com>
2025-10-07 23:32:04 +0800

    Update set.cpp

--- avoid_planner_pkg/lib/potential_field/set.cpp
+++ avoid_planner_pkg/lib/potential_field/set.cpp
@@ -14,11 +14,13 @@ void PotentialFieldCalculator::setGoal(double gaol_x,double goal_y,double goal_z
 
 }
 
-void PotentialFieldCalculator::setCurrentPose(double current_x,double current_y,double current_z);
+void PotentialFieldCalculator::setCurrentPose(double current_x,double current_y,double current_z){
 
     current_pose_[0]=current_x;
     current_pose_[1]=current_y;
     current_pose_[2]=current_z;
+
+}
 
 }
 
