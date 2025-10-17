2075106c9765771d2ced56fa571379ea9cffdf6a
cAtPile <799818193@qq.com>
2025-10-17 20:26:32 +0800

    Create goalCB.cpp

---
+++ avoid_planner_pkg/lib/goalCB.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * @brief
+ * @details
+ * @author
+ * @date
+ */
+#include"avoid_planner_pkg/avoid_planner.h"
+
+/**
+ * @brief gaolCB.cpp
+ * @details 获得目标位置
+ *          获得当前位置
+ *          计算相对极坐标位置
+ *          生成势场图
+ *          计算目标方向
+ * @see generatePFpotmap();
+ */
