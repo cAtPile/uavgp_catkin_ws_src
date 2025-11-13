145330ec8f01e9d4a02823bb481e7a1a6f8b954d
cAtPile <799818193@qq.com>
2025-11-13 20:14:35 +0800

    Update mission_master.h

--- mission_master_pkg/include/mission_master_pkg/mission_master.h
+++ mission_master_pkg/include/mission_master_pkg/mission_master.h
@@ -39,7 +39,7 @@
 #include <actionlib/client/simple_action_client.h>
 #include "mavros_msgs/SetMode.h"
 #include <actionlib/client/simple_action_client.h>
-#include "mission_master_pkg/GripperAction.h"
+#include "mission_master_pkg/GripAction.h"
 
 // 任务状态枚举
 enum mission_state
