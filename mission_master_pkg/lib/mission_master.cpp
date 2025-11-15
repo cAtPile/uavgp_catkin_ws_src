4a4e7a394fad4249534bd4c0ed57ba66df4d542d
cAtPile <799818193@qq.com>
2025-11-15 19:16:32 +0800

    parm

--- mission_master_pkg/lib/mission_master.cpp
+++ mission_master_pkg/lib/mission_master.cpp
@@ -7,17 +7,17 @@ MissionMaster::MissionMaster() : nh_(""), rate_(20.0),
                                  gripper_ac_("gripper_action", true)
 {
     //================初始化订阅者=======================
-    state_sub_ = nh_.subscribe<mavros_msgs::State>("/mavros/state", 10, &MissionMaster::state_cb, this);                               // 状态
-    local_pos_sub_ = nh_.subscribe<geometry_msgs::PoseStamped>("/mavros/local_position/pose", 10, &MissionMaster::local_pos_cb, this); // 位置
-    camtrack_sub_ = nh_.subscribe<mission_master_pkg::CamTrack>("/cam_tracker/info", 10, &MissionMaster::camtrack_cb, this);           // 视觉
+    state_sub_ = nh_.subscribe<mavros_msgs::State>("MAV_STATE", 10, &MissionMaster::state_cb, this);                               // 状态
+    local_pos_sub_ = nh_.subscribe<geometry_msgs::PoseStamped>("MAV_LOCAL_POSITION_POSE", 10, &MissionMaster::local_pos_cb, this); // 位置
+    camtrack_sub_ = nh_.subscribe<mission_master_pkg::CamTrack>("CAM_INFO", 10, &MissionMaster::camtrack_cb, this);                // 视觉
 
     //================初始化发布者======================
     setpoint_pub_ = nh_.advertise<geometry_msgs::PoseStamped>(
-        "/mavros/setpoint_position/local", 10);
+        "MAV_SETPOINT_POSITION_LOCAL", 10);
 
     //===============初始化服务客户端=====================
-    arming_client_ = nh_.serviceClient<mavros_msgs::CommandBool>("/mavros/cmd/arming");
-    set_mode_client_ = nh_.serviceClient<mavros_msgs::SetMode>("/mavros/set_mode");
+    arming_client_ = nh_.serviceClient<mavros_msgs::CommandBool>("MAV_CMD_ARMING");
+    set_mode_client_ = nh_.serviceClient<mavros_msgs::SetMode>("MAV_SET_MODE");
 
     // 初始化任务状态为等待起飞
     current_mission_state = WAITING_TAKEOFF_STATE;
@@ -50,6 +50,15 @@ void MissionMaster::loadParams()
 {
 
     home_pose = current_pose;
+
+    // 话题参数
+    nh_.param("MAV_state", MAV_STATE, "/mavros/state");
+    nh_.param("MAV_localPositionPose", MAV_LOCAL_POSITION_POSE, "/mavros/local_position/pose");
+    nh_.param("MAV_cmdArming", MAV_CMD_ARMING, "/mavros/cmd/arming");
+    nh_.param("MAV_setMode", MAV_SET_MODE, "/mavros/set_mode");
+    nh_.param("MAV_setpointPositionLocal", MAV_SETPOINT_POSITION_LOCAL, "/mavros/setpoint_position/local");
+
+    nh_.param("CAM_info", CAM_INFO, "/cam_tracker/info");
 
     // 飞行参数
     nh_.param("tolerance_waypoint", TOLERANCE_WAYPOINT, 0.10);
