fd53fea214b5e71c5f835077a00e4f875fac6fb9
cAtPile <799818193@qq.com>
2025-11-16 15:58:38 +0800

    Update mission_master.h

--- mission_master_pkg/include/mission_master_pkg/mission_master.h
+++ mission_master_pkg/include/mission_master_pkg/mission_master.h
@@ -41,6 +41,8 @@
 #include <actionlib/client/simple_action_client.h>
 #include "mission_master_pkg/GripAction.h"
 #include <mission_master_pkg/CamTrack.h>
+#include <std_msgs/Unit8.h>
+
 
 // 任务状态枚举
 enum mission_state
